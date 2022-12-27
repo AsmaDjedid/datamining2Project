46b4a9988cd5c98edf1bda890aea00353baaa154
LOUNISkenza <96489343+LOUNISkenza@users.noreply.github.com>
2022-12-27 22:00:13 +0100

    Update main.py

--- SVMTp/main.py
+++ SVMTp/main.py
@@ -1,6 +1,45 @@
 import numpy as np
 import pandas as pd
 
+class SVM:
+    
+    def __init__(self, learning_rate=1e-3, lambda_param=1e-2, nbr_iters=1000):
+        self.lr = learning_rate
+        self.lambda_param = lambda_param
+        self.n_iters = nbr_iters
+        self.w = None
+        self.b = None
+
+    def _init_weights_bias(self, x):
+        n_features = x.shape[1]
+        self.w = np.zeros(n_features)
+        self.b = 0
+
+    def _get_cls_map(self, y):
+        return np.where(y <= 0, -1, 1)
+
+    
+    def _satisfy_constraint(self, x, idx):
+        linear_model = np.dot(x, self.w) + self.b
+        return self.cls_map[idx] * linear_model >= 1
+
+
+    def _get_gradients(self, constrain, x, idx):
+            
+        if constrain:
+            dw = self.lambda_param * self.w
+            db = 0
+            return dw, db
+        
+        dw = self.lambda_param * self.w - np.dot(self.cls_map[idx], x)
+        db = - self.cls_map[idx]
+        return dw, db
+
+
+    def _update_weights_bias(self, dw, db):
+        self.w -= self.lr * dw
+        self.b -= self.lr * db
+
 def convert_data(df) -> list:
     pass
 
