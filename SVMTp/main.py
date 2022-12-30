ccf8db4ff9aa44c12bae4663acd0a438bc3709a4
Yasmine <tebaniyasmine8@gmail.com>
2022-12-30 18:42:53 +0100

    update fit et predict

--- SVMTp/main.py
+++ SVMTp/main.py
@@ -39,10 +39,34 @@ class SVM:
     def _update_weights_bias(self, dw, db):
         self.w -= self.lr * dw
         self.b -= self.lr * db
+
+    def fit(self, X, y):
+        n_samples, n_features = X.shape
+
+        y_ = np.where(y <= 0, -1, 1)
+
+        self.w = np.zeros(n_features)
+        self.b = 0
+
+        for _ in range(self.n_iters):
+            for idx, x_i in enumerate(X):
+                condition = y_[idx] * (np.dot(x_i, self.w) - self.b) >= 1
+                if condition:
+                    self.w -= self.lr * (2 * self.lambda_param * self.w)
+                else:
+                    self.w -= self.lr * (
+                        2 * self.lambda_param * self.w - np.dot(x_i, y_[idx])
+                    )
+                    self.b -= self.lr * y_[idx]
+
+    def predict(self, X):
+        approx = np.dot(X, self.w) - self.b
+        return np.sign(approx)
+
 # Déclaration de fonctions
 
 def convert_data(df) -> list: 
-    
+ 
 
     y = []  #déclaration d'un tableau vide
     for index, line in df.iterrows():  
@@ -82,18 +106,7 @@ def convert_data(df) -> list:
 
     return y 
 
-   def fit(self, X, y):
-        n_samples, n_features = X.shape
-
-        y_ = np.where(y <= 0, -1, 1)
-
-        self.w = np.zeros(n_features)
-        self.b = 0
-
-        for _ in range(self.n_iters):
-            for idx, x_i in enumerate(X):
-
-
+  
 
 
 
